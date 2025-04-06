cc649c74faf2441c46fde3100ddc17bcce262f9b
Akhil <akhilakhi298@gmail.com>
2025-04-06 11:25:03 +0530

    complete changes scraped all the endpoints in multiple chunk attempts

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ import os
 
 #settings
 SHOW_BROWSER = os.getenv('SHOW_BROWSER', "False") == 'True'
-MAX_SCROLLS = int(os.getenv('MAX_SCROLLS', 50))
-MAX_WORKERS = int(os.getenv('MAX_WORKERS',2))
-SCROLL_PAUSE_TIME = float(os.getenv('SCROLL_PAUSE_TIME', 0.8))
-WAIT_TIME = float(os.getenv('WAIT_TIME', 3))
+MAX_SCROLLS = int(os.getenv('MAX_SCROLLS', 500))
+MAX_WORKERS = int(os.getenv('MAX_WORKERS',4))
+SCROLL_PAUSE_TIME = float(os.getenv('SCROLL_PAUSE_TIME', 1.8))
+WAIT_TIME = float(os.getenv('WAIT_TIME', 2))
--- main.py
+++ main.py
@@ -22,16 +22,15 @@ def process_endpoint(domain, endpoint):
     return endpoint, []
 
 def main(urls):
-    # domain_urls_map = {}
-    # with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-    #     results = list(executor.map(process_homepage, urls))
-    #     for domain, endpoints in results:
-    #         domain_urls_map[domain] = endpoints
+    domain_urls_map = {}
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        results = list(executor.map(process_homepage, urls))
+        for domain, endpoints in results:
+            domain_urls_map[domain] = endpoints
 
-    # save_urls_to_json(domain_urls_map, 'endpoint.json')
-    # save_status_to_csv([[domain, len(endpoints), 0, 0, len(endpoints)] for domain, endpoints in domain_urls_map.items()], 'initial_status_log.csv')
-    with open('endpoint.json','r',encoding='utf-8') as data:
-        domain_urls_map = json.load(data)
+    save_urls_to_json(domain_urls_map, 'endpoint.json')
+    save_status_to_csv([[domain, len(endpoints), 0, 0, len(endpoints)] for domain, endpoints in domain_urls_map.items()], 'initial_status_log.csv')
+
     product_links_map = {}
     status_log = []
 
@@ -58,9 +57,9 @@ def main(urls):
 
 if __name__ == "__main__":
     urls = [
-        "https://www.virgio.com/",
+        # "https://www.virgio.com/",
         "https://www.tatacliq.com/",
         "https://nykaafashion.com/",
-        "https://www.westside.com/",
+        # "https://www.westside.com/",
     ]
     main(urls)
